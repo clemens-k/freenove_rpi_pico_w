2c8fb933df71206b308ef499adbd11023270511b
clemens-k <clemens-k@users.noreply.github.com>
2023-04-02 03:07:20 +0200

    LED_BUILTIN already defined (in arduino-pico 3.1.0) + blink internal LED

--- C/Sketches/Sketch_01.2_Blink/Sketch_01.2_Blink.ino
+++ C/Sketches/Sketch_01.2_Blink/Sketch_01.2_Blink.ino
@@ -4,18 +4,21 @@
 * Auther      : www.freenove.com
 * Modification: 2021/10/13
 **********************************************************************/
-#define LED_BUILTIN 15
+#define EXTRA_LED 15
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
+  pinMode(EXTRA_LED, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  digitalWrite(EXTRA_LED, HIGH);
+  delay(500);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(EXTRA_LED, LOW);
+  delay(500);                       // wait for a second
 }
