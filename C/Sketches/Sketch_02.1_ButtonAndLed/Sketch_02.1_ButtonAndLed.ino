82a04fd6bdcf4ea527c480aadbefa457f9a65c7c
clemens-k <clemens-k@users.noreply.github.com>
2023-04-02 03:08:05 +0200

    PIN_LED name clash, used for LED_BUILTIN - already defined in arduino-pico 3.1.0

--- C/Sketches/Sketch_02.1_ButtonAndLed/Sketch_02.1_ButtonAndLed.ino
+++ C/Sketches/Sketch_02.1_ButtonAndLed/Sketch_02.1_ButtonAndLed.ino
@@ -4,20 +4,31 @@
 * Auther      : www.freenove.com
 * Modification: 2021/10/13
 **********************************************************************/
-#define PIN_LED    15
+
+#define PIN_REDLED    15
 #define PIN_BUTTON 13
 
 void setup() {
   // initialize digital pin PIN_LED as an output.
-  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_REDLED, OUTPUT);
   pinMode(PIN_BUTTON, INPUT);
+  
+  
+  // short LED pulse to indicate "setup OK"
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   if (digitalRead(PIN_BUTTON) == LOW) {
-    digitalWrite(PIN_LED,HIGH);
-  }else{
-    digitalWrite(PIN_LED,LOW);
+    digitalWrite(PIN_REDLED,HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(PIN_REDLED,LOW);
+    digitalWrite(LED_BUILTIN, LOW);
   }
 }
