12bfbe1d89c366b76f38bddaddc366fe58e26ac6
clemens-k <clemens-k@users.noreply.github.com>
2023-04-02 03:08:22 +0200

    PIN_LED already defined (in arduino-pico 3.1.0)

--- C/Sketches/Sketch_02.2_TableLamp/Sketch_02.2_TableLamp.ino
+++ C/Sketches/Sketch_02.2_TableLamp/Sketch_02.2_TableLamp.ino
@@ -4,14 +4,21 @@
   Auther      : www.freenove.com
   Modification: 2021/10/13
 **********************************************************************/
-#define PIN_LED    15
+#define PIN_REDLED 15
 #define PIN_BUTTON 13
 bool ledState = false;
 
 void setup() {
   // initialize digital pin PIN_LED as an output.
-  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_REDLED, OUTPUT);
   pinMode(PIN_BUTTON, INPUT);
+
+    // short LED pulse to indicate "setup OK"
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 // the loop function runs over and over again forever
@@ -19,7 +26,7 @@ void loop() {
   if (digitalRead(PIN_BUTTON) == LOW) {
     delay(20);
     if (digitalRead(PIN_BUTTON) == LOW) {
-      reverseGPIO(PIN_LED);
+      reverseGPIO(PIN_REDLED);
     }
     while (digitalRead(PIN_BUTTON) == LOW);
   }
